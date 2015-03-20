afbff9b153fb22e5d8f43c607fd437c620119f73
Jake Harris <milieu@javakat.io>
2015-03-20 12:20:23 -0500

    Create global game controller
    Should probably be abstracted out, but for now w/e

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameController : MonoBehaviour {
+
+	public List<MinigameController> minigames;
+
+	public float timeLimit;
+
+	private int score;
+	private float timer;
+
+	private TextMesh scoreText;
+	private TextMesh timerText;
+
+	// Update is called once per frame
+	void Update () {
+		score = 0;
+		foreach (MinigameController c in minigames) 
+			score += c.GetScore ();
+
+		timer += Time.deltaTime;
+		if (timer >= timeLimit) {
+			Application.Quit();
+		}
+
+		scoreText.text = "Score:\n" + score;
+		timerText.text = "Time remaining:\n" + (timeLimit - timer);
+	}
+}
