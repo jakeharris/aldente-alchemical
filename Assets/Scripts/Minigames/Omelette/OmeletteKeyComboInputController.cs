e608b962f5f6d0849b7dd54863fd8e4dcdb165ae
Jake Harris <milieu@javakat.io>
2015-04-13 10:07:02 -0500

    Create a key-combo input controller, unlikely to be completed

---
+++ Assets/Scripts/Minigames/Omelette/OmeletteKeyComboInputController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class OmeletteKeyComboInputController : MinigameInputController {
+	private KeyCombo scoopAndFlip;
+	private KeyValuePair<string, int> lastInput;
+	private string lastKeyPressed;
+
+	void Start () {
+		scoopAndFlip = new KeyCombo ();
+		lastKeyPressed = "";
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKeyDown) {
+			if(Input.inputString != "") {
+				
+			}
+		}
+	}
+	class KeyCombo {
+		KeyValuePair<string, int>[] combo;
+		public KeyCombo () {
+
+		}
+	};
+}
